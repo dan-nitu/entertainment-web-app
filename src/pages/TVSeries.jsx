8f47500f2cf2b3275a3cfc273d55a8b961f3d63d
Voodoo30fps <voodoo30fps@gmail.com>
2026-05-19 12:15:44 +0300

    tv series page

--- src/pages/TVSeries.jsx
+++ src/pages/TVSeries.jsx
@@ -1,4 +1,21 @@
-const TVSeries = () => {
-  return <div>TV Series</div>;
+import SearchInput from '../components/SearchInput';
+import MovieCard from '../components/MovieCard';
+
+const TVSeries = ({ tvSeries }) => {
+  return (
+    <div className='container'>
+      <SearchInput placeholder='Search for TV series' />
+
+      <div className='cards-section'>
+        <h2>TV Series</h2>
+        <div className='cards-list'>
+          {tvSeries.map((movie) => (
+            <MovieCard key={movie.title} movie={movie} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
+
 export default TVSeries;
